af1de0f4753fa13680c384ce15c676524aadd679
123kubra-turan <kubraturan330@gmail.com>
2025-06-03 22:38:24 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
 
 namespace Stok_Kontrol_V2
@@ -9,13 +6,31 @@ namespace Stok_Kontrol_V2
     internal static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// Uygulamanın ana giriş noktasıdır.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            // Tüm uygulama genelinde beklenmeyen hataları yakalamak için global handler eklenebilir.
+            Application.ThreadException += (sender, args) =>
+            {
+                MessageBox.Show("Beklenmeyen bir hata oluştu:\n\n" + args.Exception.Message,
+                    "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // İsteğe bağlı: loglama yapılabilir.
+            };
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+            {
+                Exception ex = args.ExceptionObject as Exception;
+                MessageBox.Show("Kritik bir hata oluştu. Uygulama kapatılıyor.\n\n" + ex?.Message,
+                    "Kritik Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // İsteğe bağlı: loglama yapılabilir.
+            };
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            // Giriş formu veya splash screen eklenmek istenirse burada gösterilebilir.
             Application.Run(new Form1());
         }
     }
